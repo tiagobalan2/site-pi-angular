ea79d49d508503984fd2736d7c3b84fddf4e7667
Rafaela Cunha <rafaelaqueen2022@gmail.com>
2024-10-17 21:58:57 +0100

    Fazendo alguns ajustes

--- src/app/component/card-depoimento-prof/card-depoimento-prof.component.spec.ts
+++ src/app/component/card-depoimento-prof/card-depoimento-prof.component.spec.ts
@@ -2,6 +2,24 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CardDepoimentoProfComponent } from './card-depoimento-prof.component';
 
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-component',
+  templateUrl: './my-component.component.html',
+  styleUrls: ['./my-component.component.css']
+})
+export class MyComponent implements OnInit {
+  isVisible = false; // Controle de visibilidade
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.isVisible = true; 
+    }, 1000);
+  }
+}
+
+
 describe('CardDepoimentoProfComponent', () => {
   let component: CardDepoimentoProfComponent;
   let fixture: ComponentFixture<CardDepoimentoProfComponent>;
